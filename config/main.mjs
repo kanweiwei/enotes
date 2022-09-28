5bde2bac9b4852dddfb1a4676205f19fa8e868f8
kanweiwei <kwwnjujlc@sina.com>
2022-09-29 00:09:19 +0800

    chore: build config

--- config/main.mjs
+++ config/main.mjs
@@ -3,6 +3,7 @@ import { Parcel } from "@parcel/core";
 let bundler = new Parcel({
   entries: "./src/main/index.ts",
   defaultConfig: "@parcel/config-default",
+  mode: process.env.NODE_ENV,
   targets: {
     main: {
       distDir: "dist",
